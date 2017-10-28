20323f8f76a53eb28aacb18a242d41b74e6ec95f
Patrick Heller 💩 <ph@wacg.de>
2017-10-28 14:46:18 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-important-startstop', function (opts) {
-    opts = opts || {};
+function isStartTag(node) {
+    if (node.type !== 'comment') {
+        return false;
+    }
+    return node.toString().includes('@important(start)');
+}
+
+function isStopTag(node) {
+    if (node.type !== 'comment') {
+        return false;
+    }
+    return node.toString().includes('@important(stop)');
+}
+
+module.exports = postcss.plugin('postcss-important-startstop', function () {
 
     // Work with options here
-
     return function (root, result) {
+        let amountOfStartTags = 0;
+        let amountOfStopTags = 0;
 
-        // Transform CSS AST here
+        root.walk(node => {
+            if (isStartTag(node)) {
+                amountOfStartTags += 1;
+            }
 
+            if (isStopTag(node)) {
+                amountOfStopTags += 1;
+            }
+
+            if (amountOfStartTags > amountOfStopTags && node.type === 'decl') {
+                // important should be added to this declaration
+                node.important = true;
+            }
+        });
+
+        if (amountOfStartTags !== amountOfStopTags) {
+            result.warn(
+                '@important(start) must be closed with a @important(stop).' +
+                'The amount of start and stop tags must be the same.' +
+                'Given @important(start) tags: ' + amountOfStartTags +
+                'Given @important(stop) tags: ' + amountOfStopTags
+            );
+        }
     };
 });
--- index.test.js
+++ index.test.js
@@ -1,19 +1,27 @@
-var postcss = require('postcss');
+const fs = require('fs');
+const postcss = require('postcss');
 
-var plugin = require('./');
+const plugin = require('./');
 
-function run(input, output, opts) {
+function equals(input, output, opts = {}, expectedWarnings = 0) {
     return postcss([ plugin(opts) ]).process(input)
         .then(result => {
             expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
+            expect(result.warnings().length).toBe(expectedWarnings);
         });
 }
 
-/* Write tests here
+function expectWarning(input, opts = {}) {
+    return equals(input, input, opts, 1);
+}
 
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it('must have the same amount of start tags and end tags', () => {
+    expectWarning('/* @important(start) */');
 });
 
-*/
+it('add important to all declaration between annotations', () => {
+    let given = fs.readFileSync('test/given.css', 'utf-8');
+    let expected = fs.readFileSync('test/expected.css', 'utf-8');
+
+    return equals(given, expected, {});
+});
